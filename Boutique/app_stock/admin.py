3a8b8dd9480cfd5fb1f293158c7e73a3b2f19ef7
tonyngobela <156413712+tonyngobela@users.noreply.github.com>
2024-06-26 19:37:59 +0100

    Delete Boutique directory

--- Boutique/app_stock/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from app_stock.models import *
-
-# Register your models here.
-admin.site.register(Produit)
-admin.site.register(Vente)
-admin.site.register(Stock)
--- Boutique/app_stock/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from app_stock import views
-
-urlpatterns = [
-    path('aa/', views.index, name='urlindex'),
-    path('ajoutven/', views.ajoutvente, name='urlajouvent'),
-    path('insert/', views.insertion, name='urlinsert'),
-    path('affiche/', views.afficheproduit, name='urlaffiche'),
-]
